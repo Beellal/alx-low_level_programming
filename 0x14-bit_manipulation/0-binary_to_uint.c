425150620400d90f6ce5dbcc8835f4bffa944543
Beellal <mahamanehabiboubilal999@gmail.com>
2024-02-09 12:38:59 -0800

    Correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num = 0;
+unsigned int num = 0;
 
-        if (!b)
-                return (0);
+if (!b)
+return (0);
 
-        for (; *b; b++)
-        {
-                if (*b != '0' && *b != '1')
-                        return (0);
-
-                num <<= 1;
-                if (*b == '1')
-                        num += 1;
-        }
-        return (num);
+for (; *b; b++)
+{
+if (*b != '0' && *b != '1')
+return (0);
+num <<= 1;
+if (*b == '1')
+num += 1;
+}
+return (num);
 }
 
