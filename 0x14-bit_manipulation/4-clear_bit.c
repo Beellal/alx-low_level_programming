fcfab384fd33e8da784eba789539aa6160c46106
Beellal <mahamanehabiboubilal999@gmail.com>
2024-02-09 13:38:56 -0800

    Update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * clear_bit - This prog sets the value of a bit to 0 at a given index
+ * @n: parameter
+ * @index: index
+ * Return: 1 if success, -1 if error
+ */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
@@ -9,4 +15,3 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	*n &= ~(1 << index);
 	return (1);
 }
-
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
@@ -16,4 +24,3 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	return (countbit);
 }
-
