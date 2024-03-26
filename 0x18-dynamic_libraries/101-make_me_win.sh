3d1bf582f6aaf5fb0f6b8afe63033bdbdc930015
Beellal <mahamanehabiboubilal999@gmail.com>
2024-03-25 17:58:57 -0700

    Last commit hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/Beellal/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+export LD_PRELOAD="$PWD/../libgiga.so"
